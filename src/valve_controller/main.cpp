75bd7c2c53075cc72b62fef1746cba969dc9bb97
Artyom Lebedev <vagran.ast@gmail.com>
2021-07-26 00:31:01 +0300

    Test logging.

--- src/valve_controller/main.cpp
+++ src/valve_controller/main.cpp
@@ -7,6 +7,7 @@ import adk.common.MessageComposer;
 
 #include <adk/log.h>
 
+
 class TestCommitter {
 public:
     constexpr bool
@@ -16,9 +17,9 @@ public:
     }
 
     void
-    Commit(const char *msg)
+    Commit(const std::string_view &msg)
     {
-        TestLog(msg);
+        LOG << msg;
     }
 };
 
@@ -29,12 +30,25 @@ public:
     {}
 };
 
+
 export int
 main(int argc, char **argv)
 {
-    Test();
-    TestLog(std::string("aaa"));
+    LOG << "before initialize";
+    adk::Log::Initialize(adk::Log::Configuration(adk::Log::Level::DEBUG));
 
-    MyComposer() << "bbb " << 42 << " " << 3.14 << " " << -1_sz << " " << true << " " << argv;
+    using namespace std::string_literals;
+
+    Test();
+
+    MyComposer() << "bbb "s << 42 << " " << 3.14 << " " << -1_sz << " " << true << " " << argv;
     MyComposer() << false;
+
+    adk::Logger log = adk::Log::GetLoggerS("MyLogger");
+    log.Write(adk::Log::Level::INFO, "test");
+
+    LOG.Warning() << "Test warning " << 43;
+
+    adk::Log::Shutdown();
+    LOG << "after shutdown";
 }
