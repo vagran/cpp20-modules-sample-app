198385271c05c220a397292a8361e00d4a156cbd
Artyom Lebedev <vagran.ast@gmail.com>
2021-08-04 13:42:03 +0300

    ADK Gradle plugin test.

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -1,7 +1,27 @@
 plugins {
-    id("io.github.vagran.adk.gradle").version("1.0")
+    id("io.github.vagran.adk.gradle")
 }
 
 greeting {
     who = "Bob"
+    AddModule("qqqq")
 }
+
+greeting {
+    who = "Alice"
+    AddModule("wwwww")
+}
+
+adk {
+    buildType = "debug"
+    define("S1", "S2")
+    println(define)
+    define("S3", "S4")
+    println(define)
+    include("aaa")
+    println(include)
+}
+
+println(adk.binName)
+
+apply(from = "valve_controller/tmp/module.gradle")
