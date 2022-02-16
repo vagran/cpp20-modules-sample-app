019287cd105231437ad1963c5c40e05177c414d0
Artyom Lebedev <vagran.ast@gmail.com>
2022-02-16 11:59:39 +0200

    Clang-13 settings in vscode project.

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -8,7 +8,9 @@ adk {
     buildType = "debug"
     modules("sample_app", "modules/adk/src/adk", "modules/asio")
     binName = "sample_app"
-    cflags("-pthread")
+    // "-fretain-comments-from-system-headers" required for clangd to work with PCMs but currently
+    // causes clang 12 to crash during compilation, works with clang 13
+    cflags("-pthread", "-fretain-comments-from-system-headers")
     linkflags("-pthread")
     cppModuleIfaceExt = listOf("cppm")
 }
