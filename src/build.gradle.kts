b9e6bd8f6ff76a03377bc956c75fe6212c6e51d4
Artyom Lebedev <vagran.ast@gmail.com>
2022-02-16 10:32:50 +0200

    Project update.

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 adk {
     buildType = "debug"
     modules("valve_controller", "modules/adk/src/adk", "modules/asio")
+    binName = "valve_controller"
     cflags("-pthread")
     linkflags("-pthread")
     cppModuleIfaceExt = listOf("cppm")
